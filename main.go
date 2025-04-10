6bc73873029face82fa87eb10092416f0c9ea651
Chris Zavala <zavastopher@gmail.com>
2025-04-10 12:40:07 -0400

    Adding in PokeAPI location-area endpoing

--- main.go
+++ main.go
@@ -10,23 +10,30 @@ import (
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(config) error
+}
+
+type config struct {
+	next     string
+	previous string
 }
 
 var commands map[string]cliCommand
+
+var conf config
 
 func cleanInput(text string) []string {
 	cleanedInput := strings.Fields(text)
 	return cleanedInput
 }
 
-func commandExit() error {
+func commandExit(conf config) error {
 	fmt.Println("\nClosing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return fmt.Errorf("Unable to close gracefully")
 }
 
-func commandHelp() error {
+func commandHelp(conf config) error {
 	helpText := "\nWelcome to the Pokedex!\nUsage:\n"
 	for _, val := range commands {
 		helpText += "\n" + val.name + ": " + val.description
@@ -36,6 +43,14 @@ func commandHelp() error {
 	if err != nil {
 		return fmt.Errorf("There was an error printing help")
 	}
+	return nil
+}
+
+func commandMap(conf config) error {
+	return nil
+}
+
+func commandMapb(conf config) error {
 	return nil
 }
 
@@ -51,6 +66,16 @@ func main() {
 			description: "Display help command",
 			callback:    commandHelp,
 		},
+		"map": {
+			name:        "map",
+			description: "Display the next 20 locations in the Pokemon world",
+			callback:    commandMap,
+		},
+	}
+
+	conf := config{
+		next:     "https://pokeapi.co/api/v2/location-area/",
+		previous: "",
 	}
 
 	scanner := bufio.NewScanner(os.Stdin)
@@ -62,7 +87,7 @@ func main() {
 
 		command, ok := commands[commandText]
 		if ok {
-			err := command.callback()
+			err := command.callback(conf)
 			if err != nil {
 				fmt.Print(err)
 			}
