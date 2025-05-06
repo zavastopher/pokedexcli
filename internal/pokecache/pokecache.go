50348539f8c9a55d3384deea4e106273303dbdda
Chris Zavala <zavastopher@gmail.com>
2025-05-05 22:33:37 -0400

    worked on the pokecache functions

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -6,11 +6,32 @@ import (
 )
 
 type Cache struct {
-	CacheMu     sync.RWMutex
-	CacheEntrie map[string]cacheEntry
+	CacheMu    sync.RWMutex
+	CacheEntry map[string]cacheEntry
+	Interval   time.Duration
 }
 
 type cacheEntry struct {
 	createdAt time.Time
 	val       []byte
+}
+
+func NewCache(interval time.Duration) Cache {
+	return Cache{
+		CacheMu:    sync.RWMutex{},
+		CacheEntry: make(map[string]cacheEntry),
+		Interval:   interval,
+	}
+}
+func Add(key string, val []byte, cache *Cache) {
+	cache.CacheEntry[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+}
+
+func Get(key string, cache *Cache) ([]byte, bool) {
+
+	val, ok := cache.CacheEntry[key]
+	return val.val, ok
 }
